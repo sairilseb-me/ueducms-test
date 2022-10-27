91a047409002ad6d321d164892c9816026180f3a
SairilSeb <cyrilseberiaga@gmail.com>
2022-10-27 20:44:08 +0800

    created a login page

--- resources/js/components/views/cms/Login.vue
+++ resources/js/components/views/cms/Login.vue
@@ -2,15 +2,42 @@
     <v-container class="main-container d-flex align-center">
         <v-row class="justify-center">
             <v-col cols="8" md="8">
-                <v-card>
+                <v-card class="pb-1">
                     <v-card-title>
                         <h2 class="font-weight-regular text-center">Login</h2>
                     </v-card-title>
+                    
+                    <v-card-text>
+                       
+                        <v-text-field append-icon="mdi-email" label="Email" outlined placeholder="Enter email..."></v-text-field>
+                        <v-text-field 
+                        :type="showPassword? 'text': 'password'" 
+                        outlined 
+                        placeholder="Enter Password..."
+                        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                        @click:append="showPassword = !showPassword"
+                        label="Password"
+                        ></v-text-field>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-row class="justify-center mb-3">
+                            <v-btn depressed class="primary px-15">Login</v-btn>
+                        </v-row>
+                    </v-card-actions>
+                    <p class="pl-3">No account yet? <router-link to="#">Register</router-link> </p>
                 </v-card>
             </v-col>
         </v-row>
     </v-container>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        showPassword: false,
+    })
+}
+</script>
 
 <style scoped>
     .main-container{
